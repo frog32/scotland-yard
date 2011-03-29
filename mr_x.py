c6e7ae56764e82bbafdaf9ee81ff6a2862dd35db
Marc Egli <frog32@me.com>
2011-03-29 20:38:08 +0200

    a little intelligence for mrX

--- mr_x.py
+++ mr_x.py
@@ -1,25 +1,50 @@
 import random
 import logging
+import networkx as nx
 
 class MrX(object):
-    
+    graph = None
+    logger = None
     def __init__(self, graph, mr_x, polices, move_cls):
-        self.graph = graph
+        MrX.graph = graph
         self.mr_x = mr_x
         self.polices = polices
         self.move_cls = move_cls
     
     def init_logger(self, level):
-        self.logger = logging.getLogger('mr_x.MrX')
-        self.logger.setLevel(level)
+        MrX.logger = logging.getLogger('mr_x.MrX')
+        MrX.logger.setLevel(level)
         ch = logging.StreamHandler()
         formatter = logging.Formatter("%(levelname)s:%(name)s: %(message)s")
         ch.setFormatter(formatter)
-        self.logger.addHandler(ch)
+        MrX.logger.addHandler(ch)
     
     def draw(self, round, who):
-        choices = self.graph.neighbors(who.get_position())
-        r = random.randint(0, len(choices)-1)
-        target = choices[r]
-        move_type = self.graph[who.get_position()][target][0]['ticket']
-        return self.move_cls(target, move_type)
+        choices = MrX.graph.neighbors(who.get_position())
+        MrX.logger.debug('Start Move from %s' % who.get_position())
+        MrX.logger.debug('choices =%s'% choices)
+        police_positions = list(p.get_position() for p in self.polices)
+        choices = list(Choice(who.get_position(), target, police_positions) for target in choices)
+        choices.sort()
+        choice = choices[-1]
+        MrX.logger.debug('move to %s by %s' % (choice.target, choice.move_type))
+        return self.move_cls(choice.target, choice.move_type)
+
+class Choice(object):
+    def __init__(self ,position , target, police_positions):
+        self.target = target
+        self.distances = []
+        MrX.logger.debug('evaluating %s' % target)
+        for police_position in police_positions:
+            self.distances.append(nx.shortest_path_length(MrX.graph, target, police_position))
+        self.min_distance = min(self.distances)
+        MrX.logger.debug('minimum distance to police %d' % self.min_distance)
+        # allways choose the first ticket
+        self.move_type = MrX.graph[position][self.target][0]['ticket']
+        
+    def __cmp__(self, other):
+        if not isinstance(other, Choice):
+            raise
+        min_distance_diff = self.min_distance - other.min_distance
+        # if min_distance != 0:
+        return min_distance_diff
