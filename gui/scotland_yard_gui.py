ba508c11be19a6eca28d9b600a577b07c003b646
sandor <sandor@kan.com>
2011-05-07 23:37:16 +0200

    now its possible to move points

--- gui/scotland_yard_gui.py
+++ gui/scotland_yard_gui.py
@@ -38,7 +38,10 @@ class SY_GUI(object):
 
 
         #punkt 1
-        self.myCanvas.create_oval(130,15,150,35,fill="black")      
+        #self.punkt = self.myCanvas.create_oval(130,15,150,35,fill="black")      
+        s = self.stations[1]
+        s.punkt = self.myCanvas.create_oval(s.x1,s.y1,s.x2,s.y2,fill="black")
+
         """
         label = Label(self.myContainer1, image=photo)
         label.image = photo
@@ -53,21 +56,36 @@ class SY_GUI(object):
         stations = [None]
 
         for s in station_list:
-            #stations.append(self.Station(int(s.split(" ")[1]),int(s.split(" ")[2]))
-            stations.append(self.Station(int("12",13))
+            stations.append(self.Station(int(s.split(" ")[1]),int(s.split(" ")[2]),"Station " + s.split(" ")[0]))
+            
+            #stations.append(self.Station(int("12"),int("13")))
                             
         return stations
         
         
     def move(self,von,nach):
-        pass
+        
+        v = self.stations[von].punkt
+        n = self.stations[nach]
+        
+        self.myCanvas.delete(v)
+        n.punkt = self.myCanvas.create_oval(n.x1,n.y1,n.x2,n.y2,fill="black")
 
     class Station(object):
-        def __init__(self,x1,y1):
+        def __init__(self,x1,y1,name):
             self.x1 = x1
             self.y1 = y1
             self.x2 = x1 + 20
             self.y2 = y1 + 20
+            self.name = name
+            self.punkt = None
+
+        def __str__(self):
+            return self.name
+
+        def __repr__(self):
+            return self.name
+        
 
 """
 root = Tk()
