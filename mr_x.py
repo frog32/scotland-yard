7eee206d4a88928b2ff0b1e75671a520b60d56b2
Marc Egli <frog32@me.com>
2011-06-13 22:13:34 +0200

    made distances lazy and fixed some sorting things

--- mr_x.py
+++ mr_x.py
@@ -2,7 +2,7 @@ import random
 import logging
 import networkx as nx
 import copy
-
+import ConfigParser
 
 class MrX(object):
     graph = None
@@ -15,16 +15,22 @@ class MrX(object):
         self.polices = polices
         MrX.move_cls = move_cls
         MrX.logger = logger
+        # read config
+        self.config = ConfigParser.ConfigParser()
+        self.config.read('mr_x.cfg')
+        # create status tree
         self.status = Status(PlayerLight(self.mr_x.get_position()),list(PlayerLight(police.get_position()) for police in self.polices),-1)
-        self.status.build_levels(6)
-        MrX.logger.info('Mr.X init complete. created %d status and avoided %d' % (Status.count_created, Status.count_avoided))
+        self.status.build_levels(int(self.config.get('Precalculate','initial')))
+        MrX.logger.debug('Mr.X init complete. created %d status and avoided %d, created %d players' % (Status.count_created, Status.count_avoided, PlayerLight.count_created))
         # self.status.recursive_dump()
         
     def draw(self, round, who):
         MrX.logger.info('Start Move from %s' % who.get_position())
         self.status = self.status.find_current_child(self.mr_x, self.polices)
-        target, ticket = self.status.get_securest_next_move()
-        MrX.logger.info('Try moving to %s by %s' % (target, ticket))
+        target, ticket = self.status.get_securest_next_move(int(self.config.get('Precalculate','normal')))
+        self.status = self.status.find_current_child(self.mr_x, self.polices)
+        MrX.logger.info("Next Move minimum distance is %d" % self.status.distances[0])
+        MrX.logger.debug('created %d status and avoided %d, created %d players' % (Status.count_created, Status.count_avoided, PlayerLight.count_created))
         return self.move_cls(target, ticket)
 
 class Status(object):
@@ -42,16 +48,23 @@ class Status(object):
         self.mr_x = mr_x
         self.polices = polices
         self.next_player = next_player
-        self.distance = self.calculate_min_distance()
+        self._distances = [] # placeholder for lazy distances
         MrX.status_count +=1
+    
+    @property
+    def distances(self):
+        if len(self._distances):
+            return self._distances
+        self._distances = self.calculate_min_distance()
+        return self._distances
             
     def calculate_min_distance(self):
         """returns the minimum distance from mrx to any police"""
         distances = []
         for police in self.polices:
             distances.append(nx.shortest_path_length(MrX.graph, self.mr_x.position, police.position))
-            MrX.logger.debug('minimum distance to police %d' % min(distances))
-        return min(distances)
+        distances.sort()
+        return distances
     
     def build_levels(self, level_count):
         """build n levels recursive"""
@@ -78,25 +91,24 @@ class Status(object):
                 Status.count_avoided += 1
                 continue
             ticket = MrX.graph[next.position][target][0]['ticket']
-            status = Status(copy.copy(self.mr_x), copy.deepcopy(self.polices),(self.next_player+2)%(len(self.polices)+1)-1)
+            status = Status(self.mr_x, self.polices,(self.next_player+2)%(len(self.polices)+1)-1)
             if self.next_player == -1:
-                status.mr_x.position = target
-                status.mr_x.last_ticket = ticket
+                status.mr_x = PlayerLight(target,ticket)
             else:
-                status.polices[self.next_player].position = target
-                status.polices[self.next_player].last_ticket = ticket
+                status.polices[self.next_player] = PlayerLight(target,ticket)
             self.choices.append(status)
 
     def __cmp__(self, other):
         if not isinstance(other, Status):
-            raise
-        distance_diff = self.distance - other.distance
-        # if min_distance != 0:
-        return distance_diff
+            raise Exception("can only compare with same type")
+        for i in range(len(self.polices)):
+            distance_diff = other.distances[i] - self.distances[i]
+            if distance_diff != 0:
+                return distance_diff
+        return 0
     
-    def get_securest_next_move(self):
-        if len(self.choices) == 0:
-            self.build_levels(6)
+    def get_securest_next_move(self, levels_to_build):
+        self.build_levels(max(levels_to_build,1))
         self.choices.sort()
         return self.choices[0].get_mr_x_move()
     
@@ -120,28 +132,28 @@ class Status(object):
             status.recursive_dump(max_levels-1, level+1)
     
     def find_current_child(self,mr_x, polices):
-        if self.mr_x.get_position() == mr_x.get_position():
+        if self.mr_x.position == mr_x.get_position():
             match = True
             for i in range(len(self.polices)):
-                if self.polices[i].get_position() != polices[i].get_position():
+                if self.polices[i].position != polices[i].get_position():
                     match = False
                     break
             if match:
                 return self
         for status in self.choices:
             if self.next_player == -1:
-                if status.mr_x.get_position() == mr_x.get_position():
+                if status.mr_x.position == mr_x.get_position():
                     return status.find_current_child(mr_x, polices)
             else:
-                if status.polices[self.next_player].get_position() == polices[self.next_player].get_position():
+                if status.polices[self.next_player].position == polices[self.next_player].get_position():
                     return status.find_current_child(mr_x, polices)
         MrX.logger.error("didn't find current state")
+        return Status(PlayerLight(mr_x.get_position()),list(PlayerLight(police.get_position()) for police in polices), -1)
 
 class PlayerLight(object):
+    count_created = 0 # object counter
     def __init__(self, start_position, last_ticket=''):
+        PlayerLight.count_created += 1
         self.position = start_position
         self.last_ticket = last_ticket
-    
-    def get_position(self):
-        return self.position
     
